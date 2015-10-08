3fd21269820f0f0d18bfc9e22ed410b10fc16e38
Daniel Hartnell & Yasemin Celik <student@epicodus.com>
2015-10-07 19:03:55 -0700

    add dom manipulation by removing li tags.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul").prepend("<li>Hello!</li>");
+    $("ul").prepend("<li id='hello'>Hello!</li>");
+    $('ul#user').children('li').first().click(function() {
+      $(this).remove();
+      $('ul#webpage').children('li').first().remove();
+    });
   });
 
   $("button#goodbye").click(function() {
     $("ul").prepend("<li>Goodbye!</li>");
+    $('ul#user').children('li').first().click(function() {
+      $(this).remove();
+      $('ul#webpage').children('li').first().remove();
+    });
   });
 
   $("button#stop").click(function() {
     $("ul").prepend("<li>Stop copying me!</li>");
+    $('ul#user').children('li').first().click(function() {
+      $(this).remove();
+      $('ul#webpage').children('li').first().remove();
+    });
   });
 });
